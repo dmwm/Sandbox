48d9a169d42b74cba23c48cee64ba23fe8b99442
Andrew (roundabout) Melo <andrew.melo@gmail.com>
2012-02-19 21:10:12 +0100

    Squashed commit of the following:
    commit 8821d000e986c2c399ea8ff8b4a5d1bfdf6a2f09
    Merge: 1674128 4ef420a
    Author: Andrew Melo <andrew.melo@gmail.com>
    Date:   Sun Feb 19 14:00:27 2012 -0600
    
        merging with upstream
    
    commit 1674128c90d4942631b632b755964448304d80d7
    Author: Andrew Melo <andrew.melo@gmail.com>
    Date:   Sun Feb 19 13:57:26 2012 -0600
    
        Adding test-slicing to the test harness, letting the suite get split across different slaves
    
    commit 04b2f720424d75c5706c519e19cce8d2ee52751f
    Author: Andrew Melo <andrew.melo@gmail.com>
    Date:   Fri Feb 17 14:17:35 2012 -0600
    
        swampdongin
    
    commit fd39debaa7a024f1d5307b1189f3ab923a2272fe
    Author: Andrew Melo <andrew.melo@gmail.com>
    Date:   Fri Feb 17 14:07:29 2012 -0600
    
        adding banner
    
    commit 7f7d44b2fcfbc2e48f24453a87925749be345336
    Author: Andrew Melo <andrew.melo@gmail.com>
    Date:   Fri Feb 17 14:02:32 2012 -0600
    
        nose.run returns the wrong thing, oopsw
    
    commit cd898c9bdd34f098a2b2826e565324ec52322006
    Merge: 17f1677 c2969e6
    Author: Andrew Melo <andrew.melo@gmail.com>
    Date:   Fri Feb 17 13:26:29 2012 -0600
    
        Merge branch 'master' of github.com:PerilousApricot/Sandbox
    
    commit 17f1677f555717101fd7fc7211415efe2fe7405e
    Author: Andrew Melo <andrew.melo@gmail.com>
    Date:   Fri Feb 17 13:26:13 2012 -0600
    
        fixing swampdongs
    
    commit c77c9a4dbf506d320805fb0b96a92c5baea3a7be
    Author: meloam <meloam@4525493e-7705-40b1-a816-d608a930855b>
    Date:   Fri Feb 17 18:24:18 2012 +0000
    
        adding sample test stuff
    
        git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/Sandbox/trunk@15268 4525493e-7705-40b1-a816-d608a930855b
    
    commit be31298c258ed938e51df7a0601a69c6f7c2dfda
    Author: meloam <meloam@4525493e-7705-40b1-a816-d608a930855b>
    Date:   Fri Feb 17 18:24:04 2012 +0000
    
        adding gitignore
    
        git-svn-id: svn+ssh://svn.cern.ch/reps/CMSDMWM/Sandbox/trunk@15267 4525493e-7705-40b1-a816-d608a930855b

--- setup.py
+++ setup.py
@@ -1,10 +1,41 @@
 #!/usr/bin/env python2.6
 
-import nose, sys
+import nose, sys, pickle
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("--test-total-slices", dest="total_slices",
+                  help="number of slices we'll have", default = 1)
+parser.add_option("--test-current-slice", dest="current_slice",
+                  help="this slice", default = 0)
+
+(options, args) = parser.parse_args()
 
 print "This is the sandbox test harness. play nice"
+print "Processing slice id %s of %s total" % (options.current_slice, options.total_slices)
+retval = nose.run( argv=[__file__, '-q', 'tests', '-m',  '(_t.py$)|(_t$)|(^test)', '--collect-only', '--with-id'] )
+if not retval:
+    print "Failed to run nose.collectIds"
+    sys.exit(1)
 
-if nose.run( argv=[__file__, '--with-xunit', '-v','tests','-m', '(_t.py$)|(_t$)|(^test)'] ):
+idhandle = open( ".noseids", "r" )
+testIds = pickle.load(idhandle)['ids']
+idhandle.close()
+
+totalCases = len(testIds)
+myIds      = []
+for id in sorted( testIds.keys() ):
+    if ( id % int(options.total_slices) ) == int(options.current_slice):
+        myIds.append( str(id) )
+
+print "Out of %s cases, we will run %s" % (totalCases, len(myIds))
+if not myIds:
+    sys.exit(0)
+
+argv = [__file__, '--with-xunit', '-v','tests','-m', '(_t.py$)|(_t$)|(^test)', '--with-id']
+argv.extend(myIds)
+retval = nose.run( argv=argv )
+print "Got the following return value: %s" % retval
+if retval:
     sys.exit(0)
 else:
     sys.exit(1)
--- tests/dummy_t.py
+++ tests/dummy_t.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python2.6
 
 import unittest
+import new
 
 class testSomething( unittest.TestCase ):
     def testA(self):
         print "this is a test"
+    def testB(self):
+        print "this is a test"
+    def testC(self):
+        print "this is a test"
+    def testD(self):
+        print "this is a test"
+    def testE(self):
+        print "this is a test"
+    def testF(self):
+        print "this is a test"
+
